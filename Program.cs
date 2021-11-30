8e047719e3cb9d21a58afd55fd5a72c0afd20585
Brian Kuehn <brian@kuehn.se>
2021-11-30 16:22:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace pax2tlu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "";
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Error: File is missing.");
+                Console.WriteLine("Usage: pax2tlu somefile.pax");
+                Environment.Exit(-1);
+            }
+            else
+            {
+                inputFile = args[0];
+
+
+            }
+        }
+    }
+}
