92c27d7dbc7297e629cf16919e59fbb5a8e9d103
Brian Kuehn <brian@kuehn.se>
2021-12-02 10:15:58 +0100

    Adding the employee.xml file to the commandline. This file controls which employees are to be converted from the .pax file

--- Program.cs
+++ Program.cs
@@ -15,27 +15,32 @@ namespace pax2tlu
         static void ExitOnError(string message)
         {
             Console.WriteLine(message);
-            Console.WriteLine("Usage: pax2tlu somefile.pax");
+            Console.WriteLine("Usage: pax2tlu somefile.pax employee.xml");
             Environment.Exit(-1);
         }
 
         static void Main(string[] args)
         {
 
-            string inputFile;
-            XDocument paxFile, tluFile;
+            string inputFile, employeeFile;
+            XDocument paxFile, tluFile, employeeList;
             
             if (args.Length == 0)
             {
-                ExitOnError("Error: File is missing from the commandline.");               
+                ExitOnError("Error: Files are missing from the commandline.");               
             }
             else
             {
                 inputFile = args[0];
+                employeeFile = args[1];
                 if (File.Exists(inputFile))
                 {
                     if (inputFile.Contains(".pax"))
                     {
+                        if (File.Exists(employeeFile) && employeeFile.Contains(".xml"))
+                        {
+
+                        
                         paxFile = XDocument.Load(inputFile);
                         tluFile = new XDocument(
                                             new XDeclaration("1.0", "ISO-8859-1", ""),
@@ -49,6 +54,11 @@ namespace pax2tlu
                                                 );
 
                         tluFile.Save(inputFile.Replace(".pax", ".tlu"));
+                        }
+                        else
+                        {
+                            ExitOnError("Error: employee.xml file is missing");
+                        }
                     }
                     else
                     {
